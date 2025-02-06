9f85c6d1341b2bba2890cf1fbca0c453cc8b3c85
elysableu <elysabward@gmail.com>
2025-02-06 13:53:04 -0700

    Remove unnecessary lines

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::MoviesController < ApplicationController
 
   def show
-    num_results =
-
-    
-    
     results = MovieGateway.fetch_top_rated_movies( params[:total_results])
     render json: MovieSerializer.format_movie_list(results)
   end
