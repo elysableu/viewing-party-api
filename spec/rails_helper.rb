fd87914ab77a3e6714d10fd47e1136aa2037a2d3
elysableu <elysabward@gmail.com>
2025-02-05 22:23:13 -0700

    Add fixture for top_rated_movie_response

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,9 +71,9 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  config.hook_into :webmock
-  config.filter_sensitive_data('<PEXELS_API_KEY>') { Rails.application.credentials.pexels[:key] }
-  config.configure_rspec_metadata!
-end
+# VCR.configure do |config|
+#   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+#   config.hook_into :webmock
+#   config.filter_sensitive_data('<PEXELS_API_KEY>') { Rails.application.credentials.pexels[:key] }
+#   config.configure_rspec_metadata!
+# end
