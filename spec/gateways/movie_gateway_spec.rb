8eed51762323e634ebfd6f04d29943a0f7b7c5d5
elysableu <elysabward@gmail.com>
2025-02-07 09:47:46 -0700

    Add tests for movie search endpoint

--- spec/gateways/movie_gateway_spec.rb
+++ spec/gateways/movie_gateway_spec.rb
@@ -16,12 +16,54 @@ RSpec.describe MovieGateway do
           }).
         to_return(status: 200, body: json_response)
 
-    json_response = MovieGateway.fetch_top_rated_movies(20)
+    fetched_movies = MovieGateway.fetch_top_rated_movies(20)
 
-    json_response.each do |movie|
+    fetched_movies.each do |movie|
       expect(movie).to have_key :id
       expect(movie).to have_key :title
       expect(movie).to have_key :vote_average
     end
   end
+
+  it "should make a call to theMovieDB to retrieve a movie based on search query" do
+    json_response = File.read('spec/fixtures/themoviedb_top_rated_response.json')
+
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?query=Interstellar").
+        with(
+          headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Authorization'=>Rails.application.credentials.themoviedb[:key],
+        'User-Agent'=>'Faraday v2.10.1'
+          }).
+        to_return(status: 200, body: json_response)
+
+    fetched_movies = MovieGateway.fetch_movie_by_title("Interstellar")
+    movie = fetched_movies[0]
+
+    expect(movie).to have_key :id
+    expect(movie[:id]).to eq(157336)
+    expect(movie).to have_key :title
+    expect(movie[:title]).to eq("Interstellar")
+    expect(movie).to have_key :vote_average
+    expect(movie[:vote_average]).to eq(8.4)
+  end
+
+  # it "should return an error if num_results query is greater than 20" do
+  #   json_response = File.read('spec/fixtures/themoviedb_top_rated_response.json')
+
+  #   stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?total_results=20").
+  #       with(
+  #         headers: {
+  #       'Accept'=>'*/*',
+  #       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+  #       'Authorization'=>Rails.application.credentials.themoviedb[:key],
+  #       'User-Agent'=>'Faraday v2.10.1'
+  #         }).
+  #       to_return(status: 200, body: json_response)
+
+  #   expect{ MovieGateway.fetch_top_rated_movies(21) }.to raise_error()
+
+    
+  # end
 end
