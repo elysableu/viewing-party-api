52e26acbc2240cac44d4887a64616b3955ad630f
elysableu <elysabward@gmail.com>
2025-02-07 13:35:34 -0700

    Update index endpoint with conditional for different types of calls to gateway

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::MoviesController < ApplicationController
 
   def index
-    # if params[:total_results].present?
-      results = MovieGateway.fetch_top_rated_movies( params[:total_results])
+    if params[:request].present? && params[:request] == "top_rated"
+      results = MovieGateway.fetch_top_rated_movies( params[:total_results] )
       render json: MovieSerializer.format_movie_list(results)
-    # elsif params[:]
       
-    # end
+    elsif params[:request].present? && params[:request] == "search"
+      results = Movie.MovieGateway.fetch_search_movies( params[:query] )
+      render json: MovieSerializer.format_movie_list(results)
+    end
   end
 end
