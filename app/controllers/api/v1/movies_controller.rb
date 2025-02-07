dfa4a0eee08b2819c099ddf15bb567575a307b00
elysableu <elysabward@gmail.com>
2025-02-07 09:47:21 -0700

    Refactor movie_controller to use gateway

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::MoviesController < ApplicationController
 
-  def show
-    results = MovieGateway.fetch_top_rated_movies( params[:total_results])
-    render json: MovieSerializer.format_movie_list(results)
+  def index
+    # if params[:total_results].present?
+      results = MovieGateway.fetch_top_rated_movies( params[:total_results])
+      render json: MovieSerializer.format_movie_list(results)
+    # elsif params[:]
+      
+    # end
   end
 end
