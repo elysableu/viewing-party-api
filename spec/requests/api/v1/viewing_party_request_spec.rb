8e83fa22f4098be0eddaf9cd5783cf5cbdc02bfd
elysableu <elysabward@gmail.com>
2025-02-07 20:05:08 -0700

    Set up viewingPartyUsers many-to-many relationship and associated files

--- spec/requests/api/v1/viewing_party_request_spec.rb
+++ spec/requests/api/v1/viewing_party_request_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe "ViewingPartys API", type: :request do
   it { should have_many :viewing_party_users }
   it { should have_many(:user).through(:viewing_party_users)}
 
- 
+  describe "create ViewingParty endpoint" do
+    it "can create a viewingParty" do
+
+    end
+  end
 end
