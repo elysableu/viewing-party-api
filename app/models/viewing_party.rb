ffa5b8d5f6ee3db04674f1a12e3ae1933b1e50a7
elysableu <elysabward@gmail.com>
2025-02-09 17:20:56 -0700

    Added validators and invite_guest behavior to view_party model

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,17 @@
 class ViewingParty < ApplicationRecord
+  validates :name, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :movie_id, presence: true
+  validates :movie_title, presence: true
+  validates :host_id, presence: true
+
   has_many :viewing_party_users
   has_many :users, through: :viewing_party_users
+
+  def invite_guests(invitee_ids)
+    invitee_ids[:invitees].each do |id|
+      self.users << User.where(id: id)
+    end
+  end
 end
